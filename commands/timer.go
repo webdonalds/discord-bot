ffe3924e81de085d3bdd637b68c72374bdff6207
zych1751 <zych1751@naver.com>
2020-07-21 07:06:53 +0900

    set timer's timezone KST

--- commands/timer.go
+++ commands/timer.go
@@ -30,8 +30,9 @@ func (c *TimerCommand) Execute(args []string, _ *discordgo.MessageCreate) (strin
 		return "시간 패턴을 파싱할 수 없습니다.", nil, nil
 	}
 
+	kst, _ := time.LoadLocation("Asia/Seoul")
 	runAt := time.Now().Add(duration)
-	msg := fmt.Sprintf("%s에 알림이 발송됩니다.", runAt.Format("2006-01-02 15:04:05"))
+	msg := fmt.Sprintf("%s에 알림이 발송됩니다.", runAt.In(kst).Format("2006-01-02 15:04:05"))
 	watcher := background.NewTimerWatcher(&runAt, args[1])
 	return msg, watcher, nil
 }
